29b4caa2a311a0d08012e1d3067524cfb65819f7
Collin Fair <cpf@cpfx.ca>
2016-09-14 07:43:09 -0400

    Pebble 2 prep

--- generate.py
+++ generate.py
@@ -7,11 +7,13 @@ NICE_NAMES = {
     "v2_0": "Steel",
     "snowy_dvt": "Time",
     "snowy_s3": "Time Steel",
-    "spalding": "Time Round"
+    "spalding": "Time Round",
+    "silk": "Pebble 2"
 }
 HW_REVS = ["ev2_4", "v1_5", "v2_0", "snowy_dvt", "snowy_s3", "spalding"]
 
 MANIFEST = json.load(open("static/packages/manifest.json", "r"))
+MANIFEST = {k: v for k, v in MANIFEST.items() if k in HW_REVS}
 for k, item in MANIFEST.items():
     item["name"] = NICE_NAMES[k]
 SORTED_MANIFEST = sorted(MANIFEST.items(), key=lambda x: HW_REVS.index(x[0]))
